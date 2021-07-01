1bf9f374b9f23b25aeadf6db6b8283777cae58ed
Nithin Rao <nramarao@hawk.iit.edu>
2021-07-01 12:05:03 -0500

    Updated packages.

--- lib/fest.dart
+++ lib/fest.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_driver/flutter_driver.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 import 'package:fest/options.dart';
 import 'package:fest/snapshot.dart';
--- lib/snapshot.dart
+++ lib/snapshot.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_driver/flutter_driver.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:fest/extensions.dart';
 import 'package:fest/options.dart';
 
